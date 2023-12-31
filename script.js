339007a91d5d457b20dfacfe29210329b9c77977
Vladosjan <150368423+Vladosjan@users.noreply.github.com>
2023-12-31 13:38:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let div = document.createElement("img"); 
+div.src = "For work.jpg";
+div.style.width = "100%";  
+div.style.height= "100%";  
+let div1 = document.createElement("div"); 
+div1.append(div); 
+document.body.append(div); 
+
+
+
